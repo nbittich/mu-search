87d546272a4005b09bb6cde7e9bb634855c6862f
Niels V <niels.vandekeybus@redpencil.io>
2022-10-25 23:44:10 +0200

    proper handling of composite indexes in the automatic update handler
    for composite indexes we make sure to first check if the resource can be linked
    to any of the related types and only if it relates to none do we proceed in
    deleting the document.
    
    if it matches a type, we figure out which subindexes are matches and build
    documents for each of them. In case of multiple matches we merge those documents
    into one document. This strategy seems to be the most sane since it is
    impossible for a user of the service to determine which subindex would "win" if
    we would only retain one.

--- lib/mu_search/automatic_update_handler.rb
+++ lib/mu_search/automatic_update_handler.rb
@@ -45,17 +45,12 @@ module MuSearch
 
         indexes = @index_manager.indexes[index_type]
         indexes.each do |_, index|
-          type_def = @type_definitions[index_type]
-          if type_def.is_composite_index?
-            index_definitions = type_def.composite_types
-          else
-            index_definitions = [type_def]
-          end
-          index_definitions.each do |index_definition|
-            rdf_types = index_definition.related_rdf_types
-            allowed_groups = index.allowed_groups
-            if document_exists_for?(allowed_groups, document_id, rdf_types)
-              @logger.info("UPDATE HANDLER") { "Document <#{document_id}> needs to be updated in index #{index.name} for '#{index_type}' and allowed groups #{allowed_groups}" }
+          index_definition = @type_definitions[index_type]
+          rdf_types = index_definition.related_rdf_types
+          allowed_groups = index.allowed_groups
+          # check if document exists for any of the types related to the (composite) index
+          if document_exists_for?(allowed_groups, document_id, rdf_types)
+            @logger.info("UPDATE HANDLER") { "Document <#{document_id}> needs to be updated in index #{index.name} for '#{index_type}' and allowed groups #{allowed_groups}" }
               @sparql_connection_pool.with_authorization(allowed_groups) do |sparql_client|
                 document_builder = MuSearch::DocumentBuilder.new(
                   tika: @tika,
@@ -63,19 +58,32 @@ module MuSearch
                   attachment_path_base: @attachment_path_base,
                   logger: @logger
                 )
-                properties = index_definition.properties
-                document = document_builder.fetch_document_to_index(uri: document_id, properties: properties)
-                @logger.debug ("UPDATE_HANDLER") { document.pretty_inspect}
-                @elasticsearch.upsert_document index.name, document_id, document
+                if index_definition.is_composite_index?
+                  # for composite indexes check for each sub type if it matches and only if it does build the document
+                  # if multiple subindexes are matched, merge the documents
+                  # TODO: verify if this is desired behaviour
+                  merged_document = {}
+                  relevant_sub_indexes_for(document_id, index_definition.composite_types, allowed_groups).each do |sub_definition|
+                    properties = sub_definition.properties
+                    document = document_builder.fetch_document_to_index(uri: document_id, properties: properties)
+                    merged_document = smart_merge(merged_document, document)
+                  end
+                  @logger.debug ("UPDATE_HANDLER") { merged_document.pretty_inspect}
+                  @elasticsearch.upsert_document index.name, document_id, merged_document
+                else
+                  properties = index_definition.properties
+                  document = document_builder.fetch_document_to_index(uri: document_id, properties: properties)
+                  @logger.debug ("UPDATE_HANDLER") { document.pretty_inspect}
+                  @elasticsearch.upsert_document index.name, document_id, document
+                end
               end
-            else
-              @logger.info("UPDATE HANDLER") { "Document <#{document_id}> (type #{index_type}) not accessible or already removed in triplestore for allowed groups #{allowed_groups}. Removing document from Elasticsearch index #{index.name} as well." }
-              begin
-                @elasticsearch.delete_document index.name, document_id
-              rescue
-                # TODO check type of error and log warning if needed
-                @logger.info  ("UPDATE HANDLER") { "Failed to delete document #{document_id} from index #{index.name}" }
-              end
+          else
+            @logger.info("UPDATE HANDLER") { "Document <#{document_id}> (type #{index_type}) not accessible or already removed in triplestore for allowed groups #{allowed_groups}. Removing document from Elasticsearch index #{index.name} as well." }
+            begin
+              @elasticsearch.delete_document index.name, document_id
+            rescue
+              # TODO check type of error and log warning if needed
+              @logger.info  ("UPDATE HANDLER") { "Failed to delete document #{document_id} from index #{index.name}" }
             end
           end
         end
@@ -83,12 +91,50 @@ module MuSearch
     end
 
     private
+
+    ##
+    # select sub indexes matching the type(s) of the provided resource
+    def relevant_sub_indexes_for(uri, composite_types, allowed_groups)
+      @sparql_connection_pool.with_authorization(allowed_groups) do |sparql_client|
+        types = sparql_client.query( "SELECT DISTINCT ?type WHERE { #{sparql_escape_uri(uri)} a ?type}").map{ |result| result["type"].to_s }
+        composite_types.select{ |sub_definition| (sub_definition.related_rdf_types & types).length > 0 }
+      end
+    end
+
     ##
     # assumes rdf_types is an array
     def document_exists_for?(allowed_groups, document_id, rdf_types)
       @sparql_connection_pool.with_authorization(allowed_groups) do |sparql_client|
         rdf_types_string = rdf_types.map{ |type| sparql_escape_uri(type)}.join(',')
         sparql_client.query "ASK {#{sparql_escape_uri(document_id)} a ?type. filter(?type in(#{rdf_types_string})) }"
+      end
+    end
+
+    ##
+    # smart_merge document
+    # 
+    def smart_merge(document_a, document_b)
+      document_a.merge(document_b) do |key, a_val, b_val|
+        if a_val.nil?
+          b_val
+        elsif b_val.nil?
+          a_val
+        elsif a_val.is_a?(Array) && b_val.is_a?(Array)
+          a_val.concat(b_val).uniq
+        elsif a_val.is_a?(Array) && (b_val.is_a?(String) || b_val.is_a?(Integer) || b_val.is_a?(Float))
+          [*a_val, b_val].uniq
+        elsif a_val.is_a?(Hash)
+          # b_val must also be a hash
+          smart_merge(a_val, b_val)
+        elsif  b_val.is_a?(Array) && (b_val.is_a?(String) || b_val.is_a?(Integer) || b_val.is_a?(Float))
+          # a_val can not be nil or an array, so must be a simple value
+          [*b_val, a_val].uniq
+        elsif (a_val.is_a?(String) || a_val.is_a?(Integer) || a_val.is_a?(Float)) &&
+              (b_val.is_a?(String) || b_val.is_a?(Integer) || b_val.is_a?(Float))
+          [a_val,b_val].uniq
+        else
+          raise "smart_merge: Invalid combo #{a_val.inspect} and #{b_val.inspect} can not be merged"
+        end
       end
     end
   end
